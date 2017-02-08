78c03d3d570db0bc3889f1771371683c2616e6ea
InKenKa <yinfork@foxmail.com>
2017-02-08 17:13:56 +0800

    [MiniImageLoader]使用Job类来封装图片请求任务

--- app/src/main/java/com/inkenka/miniimageloader/MiniImageLoader.java
+++ app/src/main/java/com/inkenka/miniimageloader/MiniImageLoader.java
@@ -11,70 +11,26 @@ import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+/**
+ * 图片框架API接口类
+ */
 public class MiniImageLoader {
 
-    ImageMainThreadHandler mImageMainThreadHandler = new ImageMainThreadHandler();
 
     public void loadImage(final String url, final ImageView imageView){
 
-        new Thread(new Runnable() {
+        new Thread(new Job(url, new Job.MainThreadCallback() {
             @Override
-            public void run() {
-                Bitmap bitmap = requestImageFromUrl(url);
-                Targer targer = new Targer(bitmap,imageView);
-                Message msg = mImageMainThreadHandler.obtainMessage();
-                msg.obj = targer;
-                mImageMainThreadHandler.sendMessage(msg);
-            }
-        }).start();
-    }
-
-    private Bitmap requestImageFromUrl(String url) {
-        URL imgurl = null;
-        Bitmap bitmap = null;
-
-        HttpURLConnection urlConnection;
-        try {
-            imgurl = new URL(url);
-            urlConnection = (HttpURLConnection)
-                imgurl.openConnection();
-            urlConnection.setRequestMethod("GET");
-            urlConnection.connect();
-            InputStream is = urlConnection.getInputStream();
-            bitmap = BitmapFactory.decodeStream(is);
-            is.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return bitmap;
-    }
-
-    private static class Targer{
-        public Targer(Bitmap bitmap, ImageView imageView) {
-            mBitmap = bitmap;
-            mImageView = imageView;
-        }
-
-        public Bitmap mBitmap;
-        public ImageView mImageView;
-    }
-
-    private static class ImageMainThreadHandler extends Handler{
-
-        @Override
-        public void handleMessage(Message msg) {
-            super.handleMessage(msg);
-
-            if(null != msg && msg.obj instanceof Targer) {
-                Targer targer = (Targer) msg.obj;
-
-                Bitmap bitmap = targer.mBitmap;
-                ImageView imageView = targer.mImageView;
-                if(null != bitmap && null != imageView) {
+            public void onSuccessed(Bitmap bitmap) {
+                if(null != bitmap){
                     imageView.setImageBitmap(bitmap);
                 }
             }
-        }
-    }
 
+            @Override
+            public void onFailed(String errorMsg) {
+
+            }
+        })).start();
+    }
 }
